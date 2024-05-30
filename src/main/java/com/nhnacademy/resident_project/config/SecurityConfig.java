be1e4e73af889905ea5d989b8d33c3797dad2cbf
추만석 <koesnam.dev@gmail.com>
2024-05-30 15:56:29 +0900

    Update readme.md

--- src/main/java/com/nhnacademy/resident_project/config/SecurityConfig.java
+++ src/main/java/com/nhnacademy/resident_project/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.nhnacademy.resident_project.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
